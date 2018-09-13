95c2045f80fe3f1a8cc18668c376bea09072676d
sanjeev sharma <sanjeev.sharma@affluencesolutions.com>
2018-09-13 15:01:29 +0530

    Auto Manage State Using timer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ManageState from './ManageState';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ManageState />
+      </div>
+    );
+  }
+}
+export default App;
